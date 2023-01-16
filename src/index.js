5533316172a5846e9eaaf544b38d82d8d0ae0ec0
Fabio <19288282+pzzfba16@users.noreply.github.com>
2023-01-16 15:55:22 +0100

    Starting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import NavBar from "./NavBar";
 
 const rootElement = document.getElementById("root");
+const navbarElemet = document.getElementById("navbar");
+
+ReactDOM.render(<NavBar />, navbarElemet);
 ReactDOM.render(<App />, rootElement);
