e3d0dae8103352370a606456b98afa40ba4feddf
Fabio <19288282+pzzfba16@users.noreply.github.com>
2023-01-24 13:28:40 +0100

    Add Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-1 bg-light fixed-bottom">
+      <div class="container">
+        <span class="text-muted">Created by Fabio Pizzolato.</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
